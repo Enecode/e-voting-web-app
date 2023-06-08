106cbfc5230f6776fae6215f9206d5cf7d5a34b5
Isah Jacob <isahjacob0@gmail.com>
2023-06-08 23:12:19 +0100

    added urls to homepage

--- bincom_app/urls.py
+++ bincom_app/urls.py
@@ -7,6 +7,7 @@ app_name = 'bincom_app'
 urlpatterns = [
     path('', views.index, name="index"),
     path('polling_unit/<int:polling_unit_id>/', polling_unit_result, name='polling_unit_result'),
+    # path('polling_unit/<int:polling_unit_id>/', polling_unit_result, name='polling_unit_result'),
     path('lga_result/', lga_result_page, name='lga_result'),
     path('store_results/', store_results, name='store_results'),
 ]
